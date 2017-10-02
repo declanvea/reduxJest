7130a9689a7dbe1a2fc3f1a1aba2fcc232345c33
Declan Vea <declanpvea@gmail.com>
2017-10-02 17:07:52 -0400

    fixed bugs, all tests pass now

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,26 +1,28 @@
 import reducer from "./reducer";
-import { createTodo, toggleTodo, filterTodos } from "./actions";
+import * as actions from "./actions";
+
+
 
 describe("CREATE_TODO", function () {
+  const initialState = {todos: [], nextId: 1}
     test("creates a new todo", function () {
-        const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
+        const state = reducer(initialState, actions.createTodo("Test"));
         expect(state.todos).toHaveLength(1);
         expect(state.todos[0]).toEqual({id: 1, done: false, text: "Test"});
     })
 
     test("updates nextId", function () {
-        const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
+        const state = reducer(initialState, actions.createTodo("Test"));
         expect(state.nextId).toEqual(2);
     })
 })
 
 // still not working
 describe("TOGGLE_TODO", function () {
-  test('toggles a new todo', function () {
-    const initialState = {todos: [], nextId: 1}
-    const state = reducer(initialState, toggleTodo("Test"));
+  test('toggles a todo.cone (true/false)', function () {
+    const initialState = {todos: [{id: 1, done: false, text: "Test"}]};
+    const state = reducer(initialState, actions.toggleTodo(1));
+    expect(state.todos[0].done).toEqual(true);
 
 
 
@@ -29,7 +31,8 @@ describe("TOGGLE_TODO", function () {
 
 describe("FILTER_TODOS", function () {
   test('filters todo', function () {
-    const initialState = {todos: [], nextId: 1}
-    const state = reducer(initialState, filterTodos("Test"));
+    const initialState = {filter:'all'}
+    const state = reducer(initialState, actions.filterTodos('completed'));
+    expect(state.filter).toEqual('completed');
   })
 })
